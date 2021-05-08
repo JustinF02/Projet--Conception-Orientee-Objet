d1c60e808753a8af799e672d0550b07bc3b900f7
JustinF02 <justin.ferdinand54@gmail.com>
2021-05-08 16:50:39 +0200

    Méthode de déchiffrement par Justin 08/05/2021

---
+++ Projet/Carte.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projet
+{
+    class Carte
+    {
+        protected int i = 0, j = 0;
+        protected int[,] Matrice;
+        protected bool[,] Array;
+    }
+    class chiffrement : Carte
+    {
+        private int[] valeur = { 2, 3 };
+        private bool MurDeLOuest(int [,]tab) {
+            
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+
+                    if (Array.Exists(valeur, element => element == Matrice[i, j])) return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Projet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Projet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] valeur = { 2, 3 };
+            bool MurDeLOuest(int[,] tab)
+            {
+
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+
+                        if (Array.Exists(valeur, element => element == Matrice[i, j])) return true;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}

429bf64988c103da1ad8ee5abe108e9e4da10d7a
JustinF02 <justin.ferdinand54@gmail.com>
2021-05-08 16:55:32 +0200

    Version de Mael, avec lecture et affichage de la carte

--- Projet/Algo Déchiffrage Trame/obj/Debug/netcoreapp3.1/Algo Déchiffrage Trame.AssemblyInfo.cs
+++ Projet/Algo Déchiffrage Trame/obj/Debug/netcoreapp3.1/Algo Déchiffrage Trame.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Projet")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Algo Déchiffrage Trame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Projet")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Projet")]
+[assembly: System.Reflection.AssemblyProductAttribute("Algo Déchiffrage Trame")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Algo Déchiffrage Trame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
